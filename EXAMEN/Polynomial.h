b7dfcba9842224bff8153282d272e9f9dbd503b9
ChristopherFloresRimac <54603938+ChristopherFloresRimac@users.noreply.github.com>
2019-09-12 18:06:06 -0500

    6:00 pm
    6:00 pm

---
+++ EXAMEN/Polynomial.h
@@ -0,0 +1,18 @@
+//
+// Created by ruben on 9/12/2019.
+//
+
+#ifndef POLINOMIO_POLINOMIO_H
+#define POLINOMIO_POLINOMIO_H
+
+#include <vector>
+using namespace std;
+
+class Polynomial {
+private:
+    int coeficiente;
+    int variable;
+    int grado;
+};
+
+#endif //POLINOMIO_POLINOMIO_H
